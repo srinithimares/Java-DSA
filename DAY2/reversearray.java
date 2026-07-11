05c55200e18689db62749cd0fe26ac90c113eaa6
srinithimares <srinithi839@gmail.com>
2026-07-11 14:44:31 +0530

    Updated reverse array program

--- DAY2/reversearray.java
+++ DAY2/reversearray.java
@@ -10,10 +10,11 @@ public class reversearray {
             start++;
             end--;
         }
-        System.out.println("reverse array=");
+        System.out.print("reverse array= ");
         for(int i=0;i<arr.length;i++){
-        System.out.println(arr[i] +" ");
+            System.out.print(arr[i]+" ");
+        }
     }
 }
     
-}
+
