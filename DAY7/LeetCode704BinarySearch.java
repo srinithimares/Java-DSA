e2782bed348d08d0242637f350d12429e2fb01a3
srinithimares <srinithi839@gmail.com>
2026-07-18 22:36:05 +0530

    DAY7:Added LeetCode 704 Binary Search Solution

---
+++ DAY7/LeetCode704BinarySearch.java
@@ -0,0 +1,25 @@
+package DAY7;
+
+public class LeetCode704BinarySearch {
+
+    public static int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
